1c1c5752b10a29802f702fa4cff59f7381c3669e
Giraaffes <marcusfaerk@gmail.com>
2025-02-02 11:58:54 +0100

    .

--- main.ts
+++ main.ts
@@ -7,10 +7,24 @@ import * as crypto from "crypto";
 
 const server = express();
 
-server.post("/github-push", (req, res) => {
-	console.log(req);
-	res.sendStatus(200);
+server.post("/github-push", express.raw({type: "*/*"}), (req, res) => {
+	console.log(process.env.WEBHOOK_SECRET);
+
+	let ghSignatureHeader = req.headers["X-Hub-Signature-256"] as string;
+	let githubSignatureStr = (ghSignatureHeader.match(/^sha256=(.+)$/) || [])[1];
+	if (!githubSignatureStr) throw Error(`Incorrectly formatted X-Hub-Signature-256 header: ${ghSignatureHeader}`);
 	
+	let githubSignature = Buffer.from(githubSignatureStr);
+	let signature1 = crypto.createHmac("sha256", process.env.WEBHOOK_SECRET).update(req.body).digest();
+	let signature2 = crypto.createHmac("sha256", process.env.WEBHOOK_SECRET).update(req.body.toString()).digest();
+	console.log(`${githubSignature}\n${signature1}\n${signature2}`);
+
+	let areEqual1 = crypto.timingSafeEqual(signature1, githubSignature);
+	let areEqual2 = crypto.timingSafeEqual(signature2, githubSignature);
+	console.log(areEqual1, areEqual2);
+
+	res.sendStatus(200);
+
 	//if (req.headers["x-github-hook-id"] != "489728820") return;
 	//res.status(200).end();
 
