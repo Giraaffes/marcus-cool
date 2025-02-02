f0a719740e2a12aef46c1702933a9a34afaf07b1
Giraaffes <marcusfaerk@gmail.com>
2025-02-02 21:46:30 +0100

    yay it work

--- main.ts
+++ main.ts
@@ -1,12 +1,13 @@
 import "express-async-errors";
 import express from "express";
-
 import axios from "axios";
 
 import cp from "child_process";
 import crypto from "crypto";
 
-import fish from "./data/stardew_fish.json"
+import approxSearch from "approx-string-match";
+
+import fishData from "./data/stardew_fish.json";
 
 
 const server = express();
@@ -30,22 +31,67 @@ server.post("/github-push", express.raw({type: "*/*"}), (req, res) => {
 	});
 });
 
+
+function equalNoCase(str1: string, str2: string): boolean {
+	return str1.toLowerCase() == str2.toLowerCase();
+}
+
+function stringDifference(str1: string, str2: string): number {
+	let [ short, long ] = [str1, str2].sort((a, b) => a.length - b.length);
+	let matches = approxSearch(short.toLowerCase(), long.toLowerCase(), Infinity).filter(s => 
+		s.start == 0 && s.end == short.length
+	);
+	return matches.length > 0 ? matches[0].errors : Infinity;
+}
+
+function getTopMatches(values: string[], query: string, maxErrors: number): {value: string, errors: number}[] {
+	return values.map(value => 
+		({value, errors: stringDifference(value, query)})
+	).filter(m => m.errors <= maxErrors).sort((a, b) => a.errors - b.errors);
+}
+
+type Fish = typeof fishData[number];
+function formatFish(fish: Fish) {
+	let locationStr = fish.locations.join(" & ");
+	let timeStr = fish.times.length > 0 ? fish.times.join(" & ") : "anytime";
+	let seasonStr = fish.seasons.length > 0 ? `in ${fish.seasons.join(" & ")}` : "in any season";
+	let weather = fish.weather.length > 0 ? `only ${fish.weather.join(" or ")}` : "";
+	return `${fish.name} (${locationStr}, ${timeStr} ${seasonStr}${weather ? (", " + weather) : ""})`;
+}
+
+server.get("/stardew/suggestions", async (req, res) => {
+	let query = req.query.q as string;
+	let perfectMatch = fishData.find(e => equalNoCase(e.name, query));
+	if (perfectMatch) {
+		res.send([query, [formatFish(perfectMatch)], [], []])
+	} else {
+		let topMatches = getTopMatches(fishData.map(e => e.name), query, 3);
+		res.send([query, topMatches.map(m => m.value), [], []]);
+	}
+});
+
+server.get("/stardew/search", async (req, res) => {
+	let query = req.query.q as string;
+	let perfectMatch = fishData.find(e => equalNoCase(e.name, query));
+	if (perfectMatch) {
+		res.redirect(perfectMatch.url);
+	} else {
+		let bestNameMatch = getTopMatches(fishData.map(e => e.name), query, 3)[0];
+		if (bestNameMatch) {
+			let bestMatch = fishData.find(e => e.name == bestNameMatch.value);
+			res.redirect(bestMatch.url);
+		} else {
+			res.redirect(`https://stardewvalleywiki.com/mediawiki/index.php?search=${encodeURIComponent(query)}`);
+		}
+	}
+});
+
+
 server.use(express.static("static", {
 	setHeaders: res => res.set("access-control-allow-origin", "*"),
 	extensions: ["html"],
 	fallthrough: false
 }));
-
-
-//type OpenSearchSuggestions = [string, string[], string[], string[]]
-server.get("/stardew/search", async (req, res) => {
-	let query = req.query.q as string;
-	let matchedFish = fish.filter(e => e.name.startsWith(query.toLowerCase()));
-	res.send([
-		query, matchedFish.map(e => e.name), [], []
-	]);
-});
-
 
 server.use(((err, req, res, next) => {
 	if (err.status == 404) {
