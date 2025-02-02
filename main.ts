e37ed43712135d99560e13a13fae8a6f8483264c
Giraaffes <marcusfaerk@gmail.com>
2025-02-02 11:40:04 +0100

    testing webhook

--- main.ts
+++ main.ts
@@ -1,22 +1,24 @@
 import "express-async-errors";
 import * as express from "express";
 
-import { exec } from "child_process";
+import * as cp from "child_process";
+import * as crypto from "crypto";
 
 
 const server = express();
 
-console.log(Object.keys(process.env).join(", "));
+server.post("/github-push", (req, res) => {
+	console.log(req);
+	res.sendStatus(200);
+	
+	//if (req.headers["x-github-hook-id"] != "489728820") return;
+	//res.status(200).end();
 
-//server.post("/github-push", (req, res) => {
-//	if (!req.headers["x-github-hook-id"] == "489728820") return;
-//	res.status(200).end();
-
-//	exec("git pull", (error, stdout, stderr) => {
-//		console.log(stdout);
-//		process.exit();
-//	});
-//});
+	//cp.exec("git pull", (error, stdout, stderr) => {
+	//	console.log(stdout);
+	//	process.exit();
+	//});
+});
 
 server.use(express.static("files", {
 	setHeaders: res => res.set("access-control-allow-origin", "*"),
